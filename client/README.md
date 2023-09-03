84bd0521cc3411a24265fe2c13fa572a258c3b56
Steven Shyaka <sshya024@uottawa.ca>
2023-09-03 18:30:36 -0400

    to be rebased 1.

---
+++ client/README.md
@@ -0,0 +1,5 @@
+Modules:
+- SessionManager (Contains Main) (Controls user session)
+- NetworkManager (Read/Writes from Network)
+- IOManager (Read from stdin write to stdout)
+- SerializationManager (Process incoming info)
---
+++ client/SessionManager/SessionManager.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Function Prototypes
+void start();
+void stop();
+
+int main()
+{
+    start();
+    return 0;
+}
+
+/**
+ * Starts the client session.
+ */
+void start(){}
+
+/**
+ * Stops the client session.
+ */
+void stop(){}
